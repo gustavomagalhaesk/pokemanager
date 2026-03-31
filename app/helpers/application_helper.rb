eeec0ec18487020565ddaff0bf4fe12db2299636
Gustavo Magalhaes <gustavomagalhaesk@gmail.com>
2026-03-31 12:49:35 -0400

    views atualizadas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  # NÃO ESTÁ FUNCIONANDO POIS O CINZA ESTÁ SOBRESCREVENDO NO FINAL
+  def type_color(type)
+    if type.downcase.include?("grass") or type.downcase.include?("bug")
+      "border-success"
+    elsif type.downcase.include?("electric")
+      "border-warning"
+    elsif type.downcase.include?("fire")
+      "border-danger"
+    elsif type.downcase.include?("water") or type.downcase.include?("ice")
+      "border-info"
+    elsif type.downcase.include?("fairy")
+      "border-color-pink"
+    elsif type.downcase.include?("posion")
+      "border-poison"
+    elsif type.downcase.include?("ground") or type.downcase.include?("fighting")
+      "border-warning-subtle"
+    end
+  end
 end
--- app/helpers/trainers_helper.rb
+++ app/helpers/trainers_helper.rb
@@ -1,15 +1,3 @@
 module TrainersHelper
-  def type_color(type)
-    if type.downcase.include?("grass") or type.downcase.include?("bug")
-      "success"
-    elsif type.downcase.include?("electric")
-      "warning"
-    elsif type.downcase.include?("fire")
-      "danger"
-    elsif type.downcase.include?("water")
-      "info"
-    elsif type.downcase.include?("fairy")
-      "pink"
-    end
-  end
+  
 end
